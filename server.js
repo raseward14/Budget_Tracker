ca1814e7e3498c8ea67cef4f1c754d59d5629af3
Richard Seward <raseward14@gmail.com>
2021-05-09 12:15:09 -0600

    required router path routes api.js

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const routes = require("./routes/api");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
@@ -21,9 +20,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useCreateIndex: true,
   useFindAndModify: false
 });
-
-// routes
-app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
